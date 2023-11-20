4d887218948937440e6ceacc4d060f03f356d08a
Rafael Sales <2017infor45@gmail.com>
2023-11-20 16:08:06 -0300

    ajustando configuração de pastas

--- src/models/AnnotationData.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const AnnotationDataSchema = new mongoose.Schema({
-  title: String,
-  notes: String,
-  priority: Boolean,
-});
-
-module.exports = mongoose.model('Annotations', AnnotationDataSchema);
